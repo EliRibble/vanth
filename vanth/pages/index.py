e1f39bb852ffd4e960ab45ba764fe099471cf851
Eli Ribble <eli@authentise.com>
2016-06-09 10:18:30 -0600

    Use automatic template context rather than passing the path through
    Saves me having to push a bunch of common stuff through, which just
    makes sense

--- vanth/pages/index.py
+++ vanth/pages/index.py
@@ -4,4 +4,4 @@ blueprint = flask.Blueprint('index', __name__)
 
 @blueprint.route('/', methods=['GET'])
 def index():
-    return flask.render_template('index.html', path='/')
+    return flask.render_template('index.html')
